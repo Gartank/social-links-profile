55a8a2a5239ba01d06580b6df864ca06c9d24849
Fred Urbina <Fredrbina@gmail.com>
2026-05-15 17:35:01 -0400

    fi(app): fixing imports & rendering issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
-import './App.css'
+import SocialCard from "./components/SocialCard";
+import "./App.css";
+
+const user_profile = {
+	header : {
+		name: "Jessica Randall",
+		location: "London, United Kingdom",
+		desc: "Front-end developer and avidd reader.",
+		urlImg: "./assets/images/avatar-jessica.jpeg",
+	},
+	links: [
+		{ name: "github", url: "" },
+		{ name: "Fronten Mentor", url: "" },
+		{ name: "LinkedIn", url: "" },
+		{ name: "Twitter", url: "" },
+		{ name: "Instagram", url: "" },
+	],
+};
 
 function App() {
-
-  return (
-    <>
-
-    </>
-  )
+	return (
+		<>
+			<SocialCard {...user_profile} />
+		</>
+	);
 }
 
-export default App
+export default App;
