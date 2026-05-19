863aaec85597efe5198be93bef4ea3f97118abb0
Fred Urbina <Fredrbina@gmail.com>
2026-05-19 14:15:39 -0400

    deploy(package.json , vite.config.js): adding base in vite.config & predeploy and deploy in package.json

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/social-links-profile/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
